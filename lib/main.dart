60acbbafdbb604daa2e0e4e9ccde61f7aa2f667f
Collin <42948988+khemo240@users.noreply.github.com>
2019-08-09 11:43:04 -0700

    fade in and out logo

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       title: "Click and Go",
       home: Splash(),
-  routes: <String, WidgetBuilder>{
+      routes: <String, WidgetBuilder>{
     'login': (BuildContext context) => RyderLogin(),
   },
 ));
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,28 +1,88 @@
 
+import 'dart:async';
 import 'package:clickandgo/screens/main.dart';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 class Splash extends StatefulWidget {
   @override
   SplashState createState() => new SplashState();
 }
 
-class SplashState extends State<Splash> {
+
+class SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+  Timer _timer;
+
+  
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 2000),
+      vsync: this,
+    );
+
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+      _controller.repeat(reverse: true);
+
+     _timer= new Timer(Duration(seconds: 10),(){
+       Navigator.push(context,
+      MaterialPageRoute(builder: (context) => Main()));
+     });
+  
+    
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _timer.cancel();
+    super.dispose();
+    
+      
+    
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new SplashScreen(
-      imageBackground: AssetImage("images/background1.png"),
-        seconds: 10,
-        navigateAfterSeconds: new Main(),
-        image: new Image.asset('images/logo.png', width: 150,),
-        backgroundColor: Colors.white,
-        styleTextUnderTheLoader: new TextStyle(),
-        photoSize: 100.0,
-        onClick: ()=>print("Flutter Egypt"),
-        loaderColor: Colors.red
-    );;
-  }
+    return new Container(
+    child: new FadeTransition(
+      opacity: _animation,
+      child:  Center(
+      child:  Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('images/logo.png'),
+        fit: BoxFit.none,
+      ),
+    ),
+    ) ,
+    ),
+    ),
+    color: Colors.white,
 
+  );
+
+    //  new SplashScreen(
+    //     imageBackground: AssetImage("images/background1.png"),
+    //     seconds: 10,
+    //     navigateAfterSeconds: new Main(),
+    //     image: new Image.asset('images/logo.png', width: 150,),
+    //     backgroundColor: Colors.white,
+    //     styleTextUnderTheLoader: new TextStyle(),
+    //     photoSize: 100.0,
+    //     onClick: ()=>print("Flutter Egypt"),
+    //     loaderColor: Colors.red,
+        
+    // );
+    
+  }
 }
+
+
